c90be3b256fef0ede80cd1d62009f029fde9857a
Shaheen Gandhi <shaheen@oculus.com>
2019-07-04 00:46:58 -0400

    remove unused code

--- src/fiber/events.js
+++ src/fiber/events.js
@@ -2,43 +2,6 @@ import * as ScreenEventTypes from './ScreenEventTypes';
 import SyntheticKeyboardEvent from './SyntheticKeyboardEvent';
 import SyntheticFocusEvent from './SyntheticFocusEvent';
 
-const startCase = (string) => (
-  string.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match) {
-    return (+match === 0) ? "" : match.toUpperCase();
-  }).replace(/[^A-Za-z0-9 ]+/, "")
-)
-
-const blacklist = [
-  'adopt',
-  'attach',
-  'destroy',
-  'reparent',
-  'parsed content',
-  'set content',
-];
-const eventName = event => `on${startCase(event)}`;
-
-const eventListener = (node, event, ...args) => {
-  if (node._updating) return;
-
-  const handler = node.props[eventName(event)];
-
-  /*
-  if (blacklist.indexOf(event) === -1) {
-    if (handler) {
-      console.log(event, ': ', startCase(event).replace(/ /g, ''));
-    }
-  }
-  */
-
-  if (typeof handler === 'function') {
-    if (event === 'focus' || event === 'blur') {
-      args[0] = node;
-    }
-    handler(...args);
-  }
-};
-
 const eventSubscriptionMappings = {};
 const eventRegistrationNameToConfig = {};
 const syntheticEventDispatchConfigs = {};
